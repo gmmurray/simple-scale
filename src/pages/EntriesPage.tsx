fa6f7b5b512635dd46504018f349218d60d7c5f1
gmmurray <trinilime123@live.com>
2023-09-22 11:47:18 -0400

    improve entries page ux with table, deletions, and weight goal styling closes #8 and closes #9

--- src/pages/EntriesPage.tsx
+++ src/pages/EntriesPage.tsx
@@ -1,15 +1,20 @@
 import {
   Box,
   Button,
+  Checkbox,
   Chip,
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
   Link as MUILink,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Toolbar,
   Tooltip,
   Typography,
 } from '@mui/material';
+import { Fragment, useCallback, useState } from 'react';
 import {
   getDiffInfo,
   getOverallWeightGoal,
@@ -17,8 +22,8 @@ import {
   sortEntries,
 } from '../data/appData';
 
-import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import StarIcon from '@mui/icons-material/Star';
 import dayjs from 'dayjs';
 import { roundToOneDecimal } from '../util/mathUtil';
 import { useEntriesProvider } from '../data/entries/entriesContext';
@@ -26,7 +31,10 @@ import { useSettingsProvider } from '../data/settings/settingsContext';
 
 function EntriesPage() {
   const { settings } = useSettingsProvider();
-  const { entries, selectEntry, toggleCsvDialog } = useEntriesProvider();
+  const { entries, selectEntry, toggleCsvDialog, update } =
+    useEntriesProvider();
+
+  const [deleteIds, setDeleteIds] = useState<Set<string>>(new Set());
 
   const visibleEntries = sortEntries(entries);
   const overallWeightGoal = getOverallWeightGoal(settings);
@@ -34,77 +42,164 @@ function EntriesPage() {
     isAtOrPastWeightGoal(entry, settings),
   );
 
+  const handleUpdateDeleteIdsClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>, entryId: string) => {
+      event.stopPropagation();
+      setDeleteIds(state => {
+        const newState = new Set(state.values());
+        if (newState.has(entryId)) {
+          newState.delete(entryId);
+        } else {
+          newState.add(entryId);
+        }
+        return newState;
+      });
+    },
+    [],
+  );
+
+  const handleToggleAllDeleteIds = useCallback(() => {
+    setDeleteIds(state => {
+      const newState = new Set<string>();
+
+      if (state.size !== entries.length) {
+        entries.forEach(e => newState.add(e.id));
+      }
+
+      return newState;
+    });
+  }, [entries]);
+
+  const handleDeleteClick = useCallback(async () => {
+    const confirmed = confirm(
+      `Are you sure you want to delete these entries? This cannot be undone`,
+    );
+    if (!confirmed) {
+      return;
+    }
+
+    const updatedEntries = entries.filter(e => !deleteIds.has(e.id));
+    await update(updatedEntries);
+    setDeleteIds(new Set<string>());
+  }, [deleteIds, entries, update]);
+
   return (
     <Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'end' }}>
-        <Button variant="outlined" onClick={toggleCsvDialog}>
-          Import/Export
-        </Button>
-        {settings.goals.weight && (
-          <MUILink
-            variant="body1"
-            color="text.secondary"
-            sx={{ ml: 'auto' }}
-            component={Link}
-            to="/settings#goals-weight"
-          >
-            {`Goal Weight: ${settings.goals.weight.end} ${settings.goals.weight.unit}`}
-          </MUILink>
-        )}
-      </Box>
-      <List>
-        <ListItem divider>
-          <Grid
-            container
-            spacing={1}
-            sx={{ color: 'text.secondary', textAlign: 'center' }}
-          >
-            <Grid item xs={3}>
-              <Typography variant="overline">Date</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="overline">Weight</Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography variant="overline">Change</Typography>
-            </Grid>
-          </Grid>
-        </ListItem>
-        {visibleEntries.map((entry, index) => {
-          const diffInfo = getDiffInfo(
-            entry,
-            visibleEntries[index + 1],
-            overallWeightGoal,
-          );
-          return (
-            <ListItem
-              key={entry.id}
-              divider={index !== visibleEntries.length - 1}
-              disablePadding
+      <Toolbar variant="dense">
+        {deleteIds.size > 0 ? (
+          <Fragment>
+            <Typography variant="body1">{`${deleteIds.size} selected`}</Typography>
+            <Button
+              sx={{ ml: 'auto' }}
+              color="error"
+              onClick={handleDeleteClick}
             >
-              <ListItemButton
-                onClick={() => selectEntry(entry)}
-                selected={firstGoalEntry && firstGoalEntry.id === entry.id}
+              Delete
+            </Button>
+          </Fragment>
+        ) : (
+          <Fragment>
+            {settings.goals.weight && (
+              <MUILink
+                variant="body1"
+                color="text.secondary"
+                component={Link}
+                to="/settings#goals-weight"
               >
-                <Grid
-                  container
-                  spacing={1}
-                  sx={{ textAlign: 'center', alignItems: 'center' }}
+                {`Goal Weight: ${settings.goals.weight.end} ${settings.goals.weight.unit}`}
+              </MUILink>
+            )}
+            <MUILink
+              component="button"
+              onClick={toggleCsvDialog}
+              sx={{ ml: 'auto' }}
+            >
+              Import/Export
+            </MUILink>
+          </Fragment>
+        )}
+      </Toolbar>
+      <TableContainer>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              {!!firstGoalEntry && (
+                <TableCell align="center" padding="checkbox"></TableCell>
+              )}
+              <TableCell align="center" padding="checkbox">
+                <Checkbox
+                  indeterminate={
+                    deleteIds.size > 0 && deleteIds.size !== entries.length
+                  }
+                  checked={deleteIds.size === entries.length}
+                  onClick={handleToggleAllDeleteIds}
+                />
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="overline" color="text.secondary">
+                  Date
+                </Typography>
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="overline" color="text.secondary">
+                  Weight
+                </Typography>
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="overline" color="text.secondary">
+                  Change
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {visibleEntries.map((entry, index) => {
+              const diffInfo = getDiffInfo(
+                entry,
+                visibleEntries[index + 1],
+                overallWeightGoal,
+              );
+              return (
+                <TableRow
+                  key={index}
+                  hover
+                  sx={{ cursor: 'pointer' }}
+                  onClick={() => selectEntry(entry)}
                 >
-                  <Grid item xs={3}>
-                    <Tooltip
-                      title={dayjs(entry.timestamp).toDate().toLocaleString()}
-                    >
-                      <span>
-                        {dayjs(entry.timestamp).toDate().toLocaleDateString()}
-                      </span>
-                    </Tooltip>
-                  </Grid>
-                  <Grid item xs={6}>
-                    {`${roundToOneDecimal(entry.value)} ${entry.unit}`}
-                  </Grid>
-                  {!!diffInfo && (
-                    <Grid item xs={3}>
+                  {!!firstGoalEntry && (
+                    <TableCell align="center" padding="checkbox">
+                      {firstGoalEntry.id === entry.id && (
+                        <Box
+                          sx={{
+                            height: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <Tooltip title="You reached your goal with this entry">
+                            <StarIcon color="primary" />
+                          </Tooltip>
+                        </Box>
+                      )}
+                    </TableCell>
+                  )}
+                  <TableCell align="center" padding="checkbox">
+                    <Checkbox
+                      onClick={event =>
+                        handleUpdateDeleteIdsClick(event, entry.id)
+                      }
+                      checked={deleteIds.has(entry.id)}
+                    />
+                  </TableCell>
+                  <TableCell align="center">
+                    {dayjs(entry.timestamp).toDate().toLocaleDateString()}
+                  </TableCell>
+                  <TableCell align="center">{`${roundToOneDecimal(
+                    entry.value,
+                  )} ${entry.unit}`}</TableCell>
+                  <TableCell align="center">
+                    {!!diffInfo && (
                       <Chip
                         label={`${
                           diffInfo.diff > 0
@@ -114,14 +209,14 @@ function EntriesPage() {
                         color={diffInfo.desirable ? 'success' : 'error'}
                         variant="outlined"
                       />
-                    </Grid>
-                  )}
-                </Grid>
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
+                    )}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Fragment>
   );
 }
